dd1b0882c39b8f2d1fc20d8f5f3f48d9d95221e4
Duncan Mays <16djm1@queensu.ca>
2019-12-01 14:02:34 -0500

    playing around with model serialization

--- server.py
+++ server.py
@@ -2,40 +2,112 @@
 5 types of packets:
 
 init W->S
-worker notifies server that of its presence
+caused  by: a worker turning on
 {
 	'type': 'init'
 }
+response: 
 
 init_resp S->W
-	The server will send this after a worker sends an init packet
-	The server must send the worker the parameters of the current model
 {
 	'type': 'init_resp'
 	'id': some kind of ID number
-	'model': the model the server wishes the worker to train
+	'model': the topology of the model the server wishes the worker to train
+	'weights': the parameters of the model the server wishes the worker to train
+	'optimizer': the optimizer the server's model uses
+	'loss': the loss the server's model uses
+	'metrics': the metrics the server's model uses
 }
 
 push W->S
-	The worker must extract parameters from its model and send it to the server
-	The server must take these parameters and incorporate them into its model
 {
 	'type': 'push'
-	'model': the output of a call to model.save()
+	'weights': the parameters of the model on the worker
 }
 
 pull W->S
-	worker requests parameters from server
-	The server must extract parameters from its model and send it to the worker
 {
 	'type': 'pull'
 }
 
 pull_resp S->W
-	server sends worker parameters
 {
 	'type': 'pull_resp'
-	'model': the output of a call to model.save()
+	'weights': the parameters of the model on the server
 }
 
 '''
+
+from keras import Sequential
+from keras import layers
+from keras.models import model_from_json
+from keras.utils import to_categorical
+from mnist import MNIST
+import numpy as np
+
+#loads datasets so that they can be used by tensorflow to train and test the neural net
+#x data represents the input to the neural net and y data represents the output
+#training data will be used to form the neural net, wheras testing data will be used to evaluate its performance once formed
+mnist_data_raw = MNIST('../../QMIND/hello_world/mnist_data/')
+x_train_raw, y_train_raw = mnist_data_raw.load_training()
+x_test_raw, y_test_raw = mnist_data_raw.load_testing()
+#numpy will fuck up the shapes of the arrays if we do them all together so we have to do them one by one
+x_train = np.array(x_train_raw)
+y_train = np.array(y_train_raw)
+x_test = np.array(x_test_raw)
+y_test = np.array(y_test_raw)
+
+### IMPORT DATA ###
+
+### 'FEATURE ENGINEERING?' ###
+
+print('formatting data')
+
+#reshaped the input data
+x_train = x_train.reshape(60000, 28*28)
+x_test = x_test.reshape(10000, 28*28)
+#changes the type of input to float since we are about to rescale it, which effectively divides it by something, which means we will need the decimal place
+x_train = x_train.astype("float32")
+x_test = x_test.astype("float32")
+#neural nets like input between 0 and 1, but images are recorded in values between 0 and 255, so we have to divide it by 255
+x_train = x_train/255.0
+x_test = x_test/255.0
+
+#the neural net will categorize the input data into 10 categories representing the 10 numbers that a single digit can be
+#this formats both the testing and training output data into 10 categories so that it can be used effectively
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+
+model = Sequential()
+
+model.add(layers.Dense(512, activation="relu", input_shape=(784,)) )
+model.add(layers.Dense(512, activation="relu"))
+model.add(layers.Dense(10, activation="softmax"))
+
+model.compile(optimizer="adam", loss="categorical_crossentropy",metrics=["accuracy"])
+
+# model.summary()
+
+model.fit(x_train, y_train, verbose = 1, epochs=1, validation_data=(x_test, y_test))
+
+model_json = model.to_json()
+
+# print(model_json)
+
+model = None
+
+try:
+	model.fit(x_train, y_train, verbose = 1, epochs=1, validation_data=(x_test, y_test))
+except:
+	print()
+	print('no dice')
+	print()
+
+model = model_from_json(model_json)
+model.compile(optimizer="adam", loss="categorical_crossentropy",metrics=["accuracy"])
+
+model.fit(x_train, y_train, verbose = 1, epochs=1, validation_data=(x_test, y_test))
+
+# model.summary()
+
