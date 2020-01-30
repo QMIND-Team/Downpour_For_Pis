394cb1d2cc67b3eddb396564b532ed1c38f2ca8b
Troy Giorshev <troygiorshev@gmail.com>
2020-01-30 16:48:24 -0500

    Fix junk worker file

--- worker.py
+++ worker.py
@@ -1,17 +1,1 @@
-# Worker
 
-import json
-from comms import client
-
-client = comms.Client()
-
-init_message = json.loads({"type": "init"})
-init_resp = client.send(init_message)
-
-
-
-
-
-# need to pull
-message = "pull"
-pull_resp = client.send(message)
