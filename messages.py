00adfb080ba614ecc89e86e749fadb9ce3ac6307
Troy Giorshev <troygiorshev@gmail.com>
2020-01-30 17:21:15 -0500

    Make json snippet, and give messages a base class
    Equality of messages is defined to be if their dictionary representations are the same

--- messages.py
+++ messages.py
@@ -2,13 +2,20 @@
 
 import json
 
-class Init():
+class Message():
+    def __init__(self, data=None):
+        self.type = ""
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+class Init(Message):
     def __init__(self, data=None):
         self.type = "init"
         if data:
             self.__dict__ = json.loads(data)
 
-class Init_Response():
+class Init_Response(Message):
     def __init__(self, data=None):
         self.type = "init_resp"
         self.id = 0
