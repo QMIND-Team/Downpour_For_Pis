ced0fc1be195102bc00bf0d436440d0744613ee7
Troy Giorshev <troygiorshev@gmail.com>
2020-01-30 23:31:54 -0500

    Complete Worker-Client Interface Example

--- worker.py
+++ worker.py
@@ -1,1 +1,20 @@
+"""Worker"""
 
+import comms.client as cl
+from messages import Init, Init_Response
+
+import json
+
+client = cl.Client()
+
+# Make a pretend thing to send through client
+data = json.dumps(Init().__dict__)
+
+resp_dict = json.loads(client.send(data))
+
+if resp_dict["type"] == "init_resp":
+    resp = Init_Response(resp_dict)
+else:
+    raise TypeError("Didn't receive a known message type")
+
+print(resp.id)
