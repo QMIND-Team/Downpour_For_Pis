d2cc569bfabef752edc0c6bd776f7ef70517c6e8
Duncan Mays <16djm1@queensu.ca>
2020-02-06 10:23:07 -0500

    an example of how to serialize numpy arrays

--- comms/serializeLibrary.py
+++ comms/serializeLibrary.py
@@ -2,17 +2,20 @@ import numpy as np
 import io, json
 import pickle
 
-def serializeNumpyArray(array):
+def serializeArray(array):
 	memfile = io.BytesIO()
 	np.save(memfile, array)
 	memfile.seek(0)
 	return json.dumps(memfile.read().decode('latin-1'))
 
+	# if(type(array) == 'list')
+
+
 def serializeAsBytes(array):
 	return pickle.dumps(array, protocol=0)
 
 
-def deserializeNumpyArray(string):
+def deserializeArray(string):
 	memfile = io.BytesIO()
 	memfile.write(json.loads(string).encode('latin-1'))
 	memfile.seek(0)
--- comms/testing.py
+++ comms/testing.py
@@ -1,3 +1,40 @@
 from client import Client
 from server import Server
-from serializeLibrary import serializeNumpyArray, deserializeNumpyArray
+from serializeLibrary import serializeArray, deserializeArray, serializeAsBytes
+import numpy as np 
+import keras
+
+model = keras.Sequential()
+
+# convolutional layers
+model.add(keras.layers.Conv2D(64, (3,3), activation='sigmoid', input_shape=(32, 32, 3)))
+model.add(keras.layers.BatchNormalization())
+model.add(keras.layers.Conv2D(64, (3,3), activation='sigmoid', input_shape=(32, 32, 3)))
+model.add(keras.layers.BatchNormalization())
+model.add(keras.layers.MaxPooling2D((2,2)))
+model.add(keras.layers.Conv2D(64, (3,3), activation='sigmoid'))
+model.add(keras.layers.BatchNormalization())
+model.add(keras.layers.Conv2D(32, (3,3), activation='sigmoid'))
+model.add(keras.layers.BatchNormalization())
+
+# the dense layers
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dense(192, activation='sigmoid'))
+model.add(keras.layers.BatchNormalization())
+model.add(keras.layers.Dense(64, activation='sigmoid'))
+model.add(keras.layers.Dense(32, activation='sigmoid'))
+model.add(keras.layers.BatchNormalization())
+model.add(keras.layers.Dense(32, activation='sigmoid'))
+model.add(keras.layers.Dense(10, activation='softmax'))
+
+model.compile(optimizer="adam", loss='categorical_crossentropy', metrics=["accuracy"])
+
+# testing the serialization of numpy arrays, like weights and gradients
+weights = model.get_weights()
+print(type(weights))
+cereal = serializeArray(weights)
+print(type(cereal))
+new_weights = deserializeArray(cereal)
+model.set_weights(new_weights)
+
+# testing the serialization of models
