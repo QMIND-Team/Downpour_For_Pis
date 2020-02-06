162693b81c772bf0f010861e24f1846f54f51be6
Duncan Mays <16djm1@queensu.ca>
2020-02-06 10:52:19 -0500

    pushed an example of getting and serializing gradients

--- comms/serializeLibrary.py
+++ comms/serializeLibrary.py
@@ -1,12 +1,13 @@
 import numpy as np
 import io, json
 import pickle
+from keras.models import load_model
 
 def serializeArray(array):
-	memfile = io.BytesIO()
-	np.save(memfile, array)
-	memfile.seek(0)
-	return json.dumps(memfile.read().decode('latin-1'))
+	memFile = io.BytesIO()
+	np.save(memFile, array)
+	memFile.seek(0)
+	return json.dumps(memFile.read().decode('latin-1'))
 
 	# if(type(array) == 'list')
 
@@ -16,9 +17,21 @@ def serializeAsBytes(array):
 
 
 def deserializeArray(string):
-	memfile = io.BytesIO()
-	memfile.write(json.loads(string).encode('latin-1'))
-	memfile.seek(0)
-	return np.load(memfile)
+	memFile = io.BytesIO()
+	memFile.write(json.loads(string).encode('latin-1'))
+	memFile.seek(0)
+	return np.load(memFile)
+
+def serializeModel(model):
+	memFile = io.BytesIO()
+	model.save(memFile)
+	memFile.seek(0)
+	return json.dumps(memFile.read().decode('latin-1'))
+
+def deserializeModel(string):
+	memFile = io.BytesIO()
+	memFile.write(json.loads(string).encode('latin-1'))
+	memFile.seek(0)
+	return load_model(memFile)
 
 
--- comms/testing.py
+++ comms/testing.py
@@ -1,40 +1,44 @@
 from client import Client
 from server import Server
-from serializeLibrary import serializeArray, deserializeArray, serializeAsBytes
+from serializeLibrary import serializeArray, deserializeArray, serializeModel
 import numpy as np 
 import keras
+import tensorflow as tf
+from keras import backend as k
 
-model = keras.Sequential()
 
-# convolutional layers
-model.add(keras.layers.Conv2D(64, (3,3), activation='sigmoid', input_shape=(32, 32, 3)))
-model.add(keras.layers.BatchNormalization())
-model.add(keras.layers.Conv2D(64, (3,3), activation='sigmoid', input_shape=(32, 32, 3)))
-model.add(keras.layers.BatchNormalization())
-model.add(keras.layers.MaxPooling2D((2,2)))
-model.add(keras.layers.Conv2D(64, (3,3), activation='sigmoid'))
-model.add(keras.layers.BatchNormalization())
-model.add(keras.layers.Conv2D(32, (3,3), activation='sigmoid'))
-model.add(keras.layers.BatchNormalization())
+model = keras.models.Sequential()
+model.add(keras.layers.Dense(12, input_dim=8, kernel_initializer='uniform', activation='relu'))
+model.add(keras.layers.Dense(8, kernel_initializer='uniform', activation='relu'))
+model.add(keras.layers.Dense(1, kernel_initializer='uniform', activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# the dense layers
-model.add(keras.layers.Flatten())
-model.add(keras.layers.Dense(192, activation='sigmoid'))
-model.add(keras.layers.BatchNormalization())
-model.add(keras.layers.Dense(64, activation='sigmoid'))
-model.add(keras.layers.Dense(32, activation='sigmoid'))
-model.add(keras.layers.BatchNormalization())
-model.add(keras.layers.Dense(32, activation='sigmoid'))
-model.add(keras.layers.Dense(10, activation='softmax'))
+# serializing weights
+# weights = model.get_weights()
+# print(type(weights))
+# cereal = serializeArray(weights)
+# print(type(cereal))
+# new_weights = deserializeArray(cereal)
+# model.set_weights(new_weights)
 
-model.compile(optimizer="adam", loss='categorical_crossentropy', metrics=["accuracy"])
-
-# testing the serialization of numpy arrays, like weights and gradients
-weights = model.get_weights()
-print(type(weights))
-cereal = serializeArray(weights)
-print(type(cereal))
-new_weights = deserializeArray(cereal)
-model.set_weights(new_weights)
+# serializing gradients
+# getting the gradient:
+# gradientObject = k.gradients(model.output, model.trainable_weights)
+# trainingExample = np.random.random((1,8))
+# sess = tf.InteractiveSession()
+# sess.run(tf.initialize_all_variables())
+# evaluated_gradients = sess.run(gradientObject,feed_dict={model.input:trainingExample})
+# # serializing them
+# cereal = serializeArray(evaluated_gradients)
+# print(type(cereal))
+# gradient = deserializeArray(cereal)
+# print(type(gradient))
+# # updating model parameters:
+# model.set_weights(model.get_weights() - gradient)
+# # proving that the model still works:
+# model.predict(trainingExample)
 
 # testing the serialization of models
+cereal = serializeModel(model)
+print(type(cereal))
+# newModel = deserializeModel(cereal)
